25ae066c6fd60ba25426ba05b5461342cec6f535
h.sato <h.sato@sasukefinlab.com>
2023-03-09 13:18:15 +0900

    my first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProjectPage from './projects/ProjectPage';
 import './App.css';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className="container">
+      <ProjectPage/>
+    </div>
   );
 }
 
