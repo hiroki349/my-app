8f8c75eb1ec41f06c4d75384c775b23ee3bc6146
h.sato <h.sato@sasukefinlab.com>
2023-03-11 09:19:37 +0900

    commit lab-25 redux with react 1st

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,15 +1,18 @@
 import React, { SyntheticEvent, useState } from "react";
 import { Project } from "./Project";
+import { useDispatch } from 'react-redux';
+import { saveProject } from './state/projectActions';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { ProjectState } from "./state/projectTypes";
 
 interface ProjectFormProps {
   project: Project;
-  onSave: (project: Project) => void;
   onCancel: () => void;
 }
 
 function ProjectForm({ 
   project: initialProject,
-  onSave,
   onCancel
 }: ProjectFormProps) {
   const [project, setProject] = useState(initialProject);
@@ -17,11 +20,12 @@ function ProjectForm({
     name: '',
     description: '',
     budget: '',
-  })
+  });
+  const dispatch = useDispatch<ThunkDispatch<ProjectState, any, AnyAction>>();
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
     if (!isValid()) return;
-    onSave(project);
+    dispatch(saveProject(project));
   };
 
   function validate(project: Project) {
